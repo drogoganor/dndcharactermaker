481fb52b4eb95b3781ec6ec007345bbef93c6cfc
David Wilson <spamdog@gmail.com>
2019-06-28 16:59:26 +1000

    Added field formatting and visual improvements

--- src/components/DnD.vue
+++ src/components/DnD.vue
@@ -13,24 +13,24 @@
       </div>
     </div>
     <div class='content has-text-left'>
-      <div class='columns'>
-        <div class='column is-2'>Level:</div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Level:</label>
         <div class='column is-1' id="level">{{ level }}</div>
       </div>
-      <div class='columns'>
-        <div class='column is-2'>XP:</div>
+      <div class='columns field'>
+        <label class='column is-2 label'>XP:</label>
         <input class='input column is-2' id="xp" type="text" v-model="output.xp">
       </div>
-      <div class='columns'>
-        <div class='column is-2'>Character Name:</div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Character Name:</label>
         <input class='input column is-4' id="charname" type="text" v-model="output.characterName">
       </div>
-      <div class='columns'>
-        <div class='column is-2'>Player Name:</div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Player Name:</label>
         <input class='input column is-4' id="playername" type="text" v-model="output.playerName">
       </div>
-      <div>
-        <div>Race:</div>
+      <div class='field'>
+        <label class='label'>Race:</label>
         <div id="character-race">
           <div class="buttons are-small has-addons">
             <button
@@ -38,13 +38,13 @@
               v-for="race in races"
               v-bind:key="race.id"
               v-on:click="setRace(race)"
-              :class="['button', { 'is-success is-selected': output.race === race.text }]"
+              :class="['button', { 'is-link is-selected': output.race === race.text }]"
             >{{ race.text }}</button>
           </div>
         </div>
       </div>
-      <div>
-        <div>Class:</div>
+      <div class='field'>
+        <label class='label'>Class:</label>
         <div id="character-class">
           <div class="buttons are-small has-addons">
             <button
@@ -52,13 +52,13 @@
               v-for="cls in classes"
               v-bind:key="cls.id"
               v-on:click="setClass(cls)"
-              :class="['button', { 'is-success is-selected': output.class === cls.text }]"
+              :class="['button', { 'is-link is-selected': output.class === cls.text }]"
             >{{ cls.text }}</button>
           </div>
         </div>
       </div>
-      <div>
-        <div>Law/Chaos:</div>
+      <div class='field'>
+        <label class='label'>Law/Chaos:</label>
         <div id="character-alignment1">
           <div class="buttons are-small has-addons">
             <button
@@ -66,13 +66,13 @@
               v-for="align in alignmentLawChaos"
               v-bind:key="align.id"
               v-on:click="output.alignmentLawChaos = align.text"
-              :class="['button', { 'is-success is-selected': output.alignmentLawChaos === align.text }]"
+              :class="['button', { 'is-link is-selected': output.alignmentLawChaos === align.text }]"
             >{{ align.text }}</button>
           </div>
         </div>
       </div>
-      <div>
-        <div>Good/Evil:</div>
+      <div class='field'>
+        <label class='label'>Good/Evil:</label>
         <div id="character-alignment2">
           <div class="buttons are-small has-addons">
             <button
@@ -80,13 +80,13 @@
               v-for="align in alignmentGoodEvil"
               v-bind:key="align.id"
               v-on:click="output.alignmentGoodEvil = align.text"
-              :class="['button', { 'is-success is-selected': output.alignmentGoodEvil === align.text }]"
+              :class="['button', { 'is-link is-selected': output.alignmentGoodEvil === align.text }]"
             >{{ align.text }}</button>
           </div>
         </div>
       </div>
-      <div>
-        <div>Background:</div>
+      <div class='field'>
+        <label class='label'>Background:</label>
         <div id="character-background">
           <div class="buttons are-small has-addons">
             <button
@@ -94,63 +94,62 @@
               v-for="bg in backgrounds"
               v-bind:key="bg.id"
               v-on:click="setBackground(bg)"
-              :class="['button', { 'is-success is-selected': output.background === bg.text }]"
+              :class="['button', { 'is-link is-selected': output.background === bg.text }]"
             >{{ bg.text }}</button>
           </div>
         </div>
       </div>
-      <div class='columns' v-if="toolProficienciesText !== ''">
-        <div class='column is-2'>Tool Proficiencies:</div>
-        <div class='column' id="toolProfs">{{ toolProficienciesText }}</div>
+      <div class='columns field' v-if="toolProficienciesText !== ''">
+        <label class='column is-2 label'>Tool Proficiencies:</label>
+        <div class='column' id="toolProfs">
+          <span class='tag is-dark'>{{ toolProficienciesText }}</span>
+        </div>
       </div>
-      <div class='columns'>
-        <div class='column is-2'>Currency:</div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Currency:</label>
         <div class='column' id="currency">{{ currencyText }}</div>
       </div>
-      <div>
-        <div>Stats:</div>
-      </div>
       <div class='columns'>
-        <div class='column is-2'>Reset Stats:</div>
-        <div class='column'>
-          <button type="button" class='button is-danger' v-on:click="standardStats">Standard Array</button>&nbsp;
-          <button type="button" class='button is-danger' v-on:click="rollStats">Reroll</button>
-        </div>
-      </div>
-      <div class='columns' v-if='statRolls.length > 0'>
-        <div class='column is-2'>Assign <b>{{ currentStatAssignmentText }}</b>:</div>
-        <div class='column buttons'>
-          <button
-            type="button"
-            class="button is-success"
-            v-for="(r, i) in statRolls"
-            v-bind:key="i"
-            v-on:click="allocateStat(r)"
-          >{{ r }}</button>
-        </div>
-      </div>
-      <div class='columns'>
-        <div class='column is-1'></div>
+        <label class='column is-2 label'>Stats:</label>
         <div class='column is-1'>Assigned</div>
         <div class='column is-1'>Racial</div>
         <div class='column is-1'>Total</div>
         <div class='column is-1'>Modifier</div>
       </div>
       <div class='columns' id="stat-block" v-for="block in statBlocks" v-bind:key="block.id">
-        <div class='column is-1'>{{ block.text }}</div>
+        <div class='column is-2'>{{ block.text }}</div>
         <div class='column is-1'>{{ output.statArray[block.id] }}</div>
         <div class='column is-1'>{{ output.raceStatBonuses[block.id] }}</div>
         <div class='column is-1'>{{ output.finalStats[block.id] }}</div>
         <div class='column is-1'>{{ formatModifier(output.statModifiers[block.id]) }}</div>
       </div>
-      <div class='columns'>
-        <div class='column is-2'>Proficiencies:</div>
+      <div class='columns field' v-if='statRolls.length > 0'>
+        <div class='column is-2'>Assign <b>{{ currentStatAssignmentText }}</b>:</div>
+        <div class='column buttons has-addons'>
+          <button
+            type="button"
+            class="button is-link"
+            v-for="(r, i) in statRolls"
+            v-bind:key="i"
+            v-on:click="allocateStat(r)"
+          >{{ r }}</button>
+        </div>
+      </div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Reset Stats:</label>
+        <div class='column'>
+          <button type="button" class='button is-danger' v-on:click="standardStats">Standard Array</button>&nbsp;
+          <button type="button" class='button is-danger' v-on:click="rollStats">Reroll</button>
+        </div>
+      </div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Proficiencies:</label>
         <div class='column' id="proficiencies">
           <div class='tags are-small'>
             <span
               v-for="prof in output.proficiencies"
               v-bind:key="prof"
-              class='tag'
+              class='tag is-dark'
               role="alert"
             >{{ skills[prof].text }}</span>
           </div>
@@ -158,11 +157,11 @@
             <button type="button" class='button is-danger' v-on:click="resetProficiencies">Reset Proficiencies</button>
           </div>
           <div v-if="!allProficienciesChosen">
-            <div>{{ proficiencesLeftText }}</div>
+            <label class='label'>{{ proficiencesLeftText }}</label>
             <div class='buttons are-small'>
               <button
                 type="button"
-                class='button is-success'
+                class='button is-link'
                 v-for="prof in availableProficiencies"
                 v-bind:key="prof.id"
                 v-on:click="addProficiency(prof.id)"
@@ -172,45 +171,46 @@
         </div>
       </div>
 
-      <div class='columns'>
-        <div class='column is-2'>Equipment:</div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Equipment:</label>
         <div class='column tags are-small'>
           <div class='tags are-small'>
-          <span v-for="eq in equipmentTextList" v-bind:key="eq" class='tag'>{{ eq }}</span>
+            <span v-for="eq in equipmentTextList" v-bind:key="eq" class='tag is-dark'>{{ eq }}</span>
+          </div>
+          <label class='label' v-if='!allEquipmentChosen'>Select Extra Equipment:</label>
+          <div v-for="choices in equipmentChoiceModel" v-bind:key="choices.id">
+            <span v-for="choice in choices.choices" v-bind:key="choice.id" class='buttons are-small'>
+                <span class='tag'>{{ choice.num }}x</span>
+                <button type="button" v-for="item in choice.items" v-bind:key="item.id" class='button is-link' v-on:click="selectEquipment(choices.id, choice.id, item)">{{ getEquipmentName(item) }}</button>
+                <span class='tag' v-if="choice.id < choices.choices.length - 1">OR</span>
+            </span>
+            <hr />
           </div>
           <div v-if="hasChosenAnyEquipment">
             <button type="button" class='button is-danger' v-on:click="resetEquipment">Reset Equipment</button>
           </div>
-          <div v-for="choices in equipmentChoiceModel" v-bind:key="choices.id">
-              <span v-for="choice in choices.choices" v-bind:key="choice.id" class='buttons are-small'>
-                  <span class='tag'>{{ choice.num }}x</span>
-                  <button type="button" v-for="item in choice.items" v-bind:key="item.id" class='button is-success' v-on:click="selectEquipment(choices.id, choice.id, item)">{{ getEquipmentName(item) }}</button>
-                  <span class='tag' v-if="choice.id < choices.choices.length - 1">OR</span>
-              </span>
-              <hr />
-          </div>
         </div>
       </div>
 
-      <div class='columns'>
-        <div class='column is-2'>Languages:</div>
+      <div class='columns field'>
+        <label class='column is-2 label'>Languages:</label>
         <div class='column tags are-small'>
           <div class='tags are-small'>
             <span
               v-for="lang in output.languageids"
               v-bind:key="lang"
-              class='tag'
+              class='tag is-dark'
             >{{ languages[lang].text }}</span>
           </div>
           <div v-if="hasBonusLanguages && allLanguagesChosen">
             <button type="button" class='button is-danger' v-on:click="resetLanguages">Reset Languages</button>
           </div>
           <div v-if="!allLanguagesChosen">
-            <div>{{ languagesLeftText }}</div>
+            <label class='label'>{{ languagesLeftText }}</label>
             <div class='buttons are-small'>
               <button
                 type="button"
-                class='button is-success'
+                class='button is-link'
                 v-for="lang in availableLanguages"
                 v-bind:key="lang.id"
                 v-on:click="addLanguage(lang.id)"
@@ -224,9 +224,9 @@
         <button
           type="button"
           id="generate"
-          class='button is-success is-large'
           v-on:click="generate"
           v-bind="generateButtonBindings"
+          :class="['button is-large', { 'is-outlined': !allChoicesFulfilled, 'is-success': allChoicesFulfilled }]"
         >Generate PDF</button>
       </div>
 
